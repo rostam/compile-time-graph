278ef5f7328bbbca0a1ab47ade156d5bc540914e
Ali Rostami <rostamiev@gmail.com>
2025-06-10 10:29:45 +0200

    Update traversal.hpp

--- include/ctgraph/traversal.hpp
+++ include/ctgraph/traversal.hpp
@@ -29,3 +29,36 @@ consteval DFSResult<NumNodes> dfs(const Graph<NumNodes, NumEdges, Undirected>& g
 
     return result;
 }
+
+template <size_t NumNodes>
+struct BFSResult {
+    std::array<bool, NumNodes> visited{};
+    std::array<size_t, NumNodes> order{};
+    size_t count = 0;
+};
+
+template <size_t NumNodes, size_t NumEdges, bool Undirected = false>
+consteval BFSResult<NumNodes> bfs(const Graph<NumNodes, NumEdges, Undirected>& graph, size_t start) {
+    BFSResult<NumNodes> result{};
+    std::array<size_t, NumNodes> queue{};
+    size_t front = 0, back = 0;
+
+    queue[back++] = start;
+    result.visited[start] = true;
+
+    while (front < back) {
+        size_t node = queue[front++];
+        result.order[result.count++] = node;
+
+        for (size_t i = 0; i < graph.sizes[node]; ++i) {
+            size_t neighbor = graph.adj[node][i];
+            if (!result.visited[neighbor]) {
+                result.visited[neighbor] = true;
+                queue[back++] = neighbor;
+            }
+        }
+    }
+
+    return result;
+}
+
